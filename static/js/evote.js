3235a015e4622963d0e987d01638866d51a48924
Massimo <massimo.dipierro@gmail.com>
2014-02-28 14:56:05 -0600

    Don't display candidate rank in ranked choice elections.

--- static/js/evote.js
+++ static/js/evote.js
@@ -1,4 +1,4 @@
 var uls=[];
-jQuery('.model-ranking').attr('readonly',true).each(function(){var ul=jQuery(this).closest('ul'); ul.find('li').addClass('ranking-option'); if(uls.indexOf(ul)<0) uls.push(ul); }); 
+jQuery('.model-ranking').attr('readonly',true).each(function(){var ul=jQuery(this).closest('ul'); ul.find('li').addClass('ranking-option').find('input').addClass('hidden'); if(uls.indexOf(ul)<0) uls.push(ul); }); 
 for(var k=0; k<uls.length; k++) uls[k].sortable({stop:function(){jQuery(this).find('input.model-ranking').each(function(i){jQuery(this).val(i+1);})} });
 
