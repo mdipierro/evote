2216c6bf9bb575b39040f98d149e1290df8980e5
mdipierro <massimo.dipierro@gmail.com>
2015-06-05 16:40:40 -0500

    minor fixes

--- models/db.py
+++ models/db.py
@@ -73,7 +73,7 @@ mail.settings.sender = myconf.take('smtp.sender')
 mail.settings.login = myconf.take('smtp.login')
 
 ## configure auth policy
-auth.settings.registration_requires_verification = False
+auth.settings.registration_requires_verification = not DEVELOPMENT
 auth.settings.registration_requires_approval = False
 auth.settings.reset_password_requires_verification = True
 
